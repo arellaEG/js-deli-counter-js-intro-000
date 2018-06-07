6c53cc55690adcf759d0a60b51305440d4471c33
Arella Gussow <arellaeg@gmail.com>
2018-06-07 05:02:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,8 @@ function currentLine(katzDeliLine){
   if (katzDeliLine.length===0){
     return `The line is currently empty.`
   }
-  
   else {
-    curLine=[]
+    var curLine=[]
     for (let i=0; i<katzDeliLine; i++){
       curLine.push(`${i+1}. ${katzDeliLine[i]}`)
     }
