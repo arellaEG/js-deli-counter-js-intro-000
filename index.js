4184aff05727188a9c04982c9fb7a6204e296672
Arella Gussow <arellaeg@gmail.com>
2018-06-07 04:27:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ function takeANumber(katzDeliLine, name){
 
 
 function nowServing(katzDeliLine){
-    for (let i=0; i<katzDeliLine; i++){
-      if (katzDeliLine.length===0){
-        return 'There is nobody waiting to be served!'}
-        else {return `Currently serving ${katzDeliLine[0]}`
-            katzDeliLine.pop(i)}
-    }
+  if (katzDeliLine.length===0){
+    return 'There is nobody waiting to be served!'
   }
+  else {
+    for (let i=0; i<katzDeliLine; i++){
+    return `Currently serving ${katzDeliLine[0]}`
+    katzDeliLine.pop(i)}
+  }
+}
