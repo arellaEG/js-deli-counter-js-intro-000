099ae0df103636975e6260a875c5189e10a0c197
Arella Gussow <arellaeg@gmail.com>
2018-06-07 04:20:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name)
   return i
 }
+
+
+function nowServing(katzDeliLine){
+  if katzDeliLine.length===0{
+    return 'There is nobody waiting to be served!'
+  }
+  else {
+    for (let i=0; i<katzDeliLine; i++){
+    return katzDeliLine[0]
+    katzDeliLine.pop(i)}
+  }
+}
